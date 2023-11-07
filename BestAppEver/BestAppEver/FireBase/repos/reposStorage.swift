e8d3efccf61d165a3bc3bb1f56246434c79f10fc
Frederic <frederic.vandevenne@gmail.com>
2023-11-07 16:18:55 +0100

    + ajout d'un guid au fichier

--- BestAppEver/BestAppEver/FireBase/repos/reposStorage.swift
+++ BestAppEver/BestAppEver/FireBase/repos/reposStorage.swift
@@ -8,40 +8,54 @@
 import Foundation
 import FirebaseStorage
 
+enum FileType: String {
+    case image
+    case objet3d
+    case autre
+}
+
+extension FileType {
+    func toString() -> String {
+        return self.rawValue
+    }
+}
+
+
 class ReposStorage {
     let storage = Storage.storage()
+    let urlStorage : String = "gs://bestappever-47608.appspot.com"
     
-    func saveFile(localURL: URL, completion: @escaping (Error?) -> Void) {
-        
-        let storageRef = storage.reference(forURL: "gs://bestappever-eee4a.appspot.com")
-        let fileName = localURL.lastPathComponent
-        let fileRef = storageRef.child("object3d/" + fileName)
+    
+    func saveFile(localURL: URL, typeFile: FileType, completion: @escaping (String?, Error?) -> Void) {
+        let storageRef = storage.reference(forURL: urlStorage)
+        let fileName = UUID().uuidString + "_" + localURL.lastPathComponent
+        let fileRef = storageRef.child(typeFile.toString() + "/" + fileName)
         let uploadTask = fileRef.putFile(from: localURL, metadata: nil) { (metadata, error) in
             if let error = error {
-                completion(error)
+                completion(nil, error)
             } else {
-                completion(nil)
+                completion(fileName, nil)
                 // Le fichier a été téléchargé avec succès
             }
         }
-        
+
         uploadTask.observe(.progress) { snapshot in
             // Mise à jour de la progression
         }
-        
+
         uploadTask.observe(.failure) { snapshot in
             if let error = snapshot.error {
-                completion(error)
+                completion(nil, error)
             }
         }
     }
-    
-    func loadFile(nameFile: String, urlLocal: String, completion: @escaping (String?, Error?) -> Void) {
-        let storageRef = storage.reference(forURL: "gs://bestappever-eee4a.appspot.com")
-        let islandRef = storageRef.child("object3d/" + nameFile)
-        
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let localURL = documentsDirectory.appendingPathComponent(nameFile)
+
+    func loadFile(typeFile: FileType, nameFile: String, urlLocal: String, completion: @escaping (String?, Error?) -> Void) {
+        let storageRef = storage.reference(forURL: urlStorage)
+        let islandRef = storageRef.child(typeFile.toString() + "/" + nameFile)
+
+        // Utilisez directement l'URL fournie dans urlLocal
+        let localURL = URL(fileURLWithPath: urlLocal)
 
         let downloadTask = islandRef.write(toFile: localURL) { url, error in
             if let error = error {
